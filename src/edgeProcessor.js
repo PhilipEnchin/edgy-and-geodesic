85fc0f9cb961a981d2bc7e7f1e929bcdbe11fde2
Philip Enchin <Philip@Ench.in>
2024-05-09 20:54:05 +0000

    Implement edge processing functions

---
+++ src/edgeProcessor.js
@@ -0,0 +1,52 @@
+/** @typedef {import('./Vector.js').default} Vector3 */
+/** @typedef {import('./Vertex.js').default} Vertex */
+
+import { vertexCompare } from './comparators.js';
+import { round } from './numberFormatters.js';
+
+/**
+ * @typedef {object} DecoratedEdge
+ * @property {[Vector3, Vector3]} vectors
+ * @property {number} edgeLength
+ * @property {string} label
+ */
+
+/**
+ * @typedef {object} GroupedEdges
+ * @property {number} edgeLength
+ * @property {DecoratedEdge[]} edges
+ */
+
+/**
+ * @param {Vertex} vertex
+ * @param {number} roundingPlace
+ * @returns {DecoratedEdge[]}
+ */
+export const decorateEdges = (vertex, roundingPlace) => vertex.edges.map(([a, b]) => {
+  const orderedVertices = vertexCompare(a, b) < 0 ? [a, b] : [b, a];
+  return {
+    vectors: [orderedVertices[0].vector3, orderedVertices[1].vector3],
+    edgeLength: round(a.vector3.distanceTo(b.vector3), roundingPlace),
+    label: orderedVertices.map((v) => v.key).join('|'),
+  };
+});
+
+/**
+ * @param {DecoratedEdge[]} decoratedEdges
+ * @returns {GroupedEdges[]}
+ */
+export const groupDecoratedEdgesByLength = (decoratedEdges) => {
+  if (decoratedEdges.length === 0) { return []; }
+
+  const sortedEdges = [...decoratedEdges].sort(({ edgeLength: a }, { edgeLength: b }) => a - b);
+
+  /** @type {GroupedEdges} */ let currentGroup = { edgeLength: sortedEdges[0]?.edgeLength, edges: [] };
+  /** @type {GroupedEdges[]} */ const groups = [currentGroup];
+  sortedEdges.forEach((decoratedEdge) => {
+    if (decoratedEdge.edgeLength === currentGroup.edgeLength) currentGroup.edges.push(decoratedEdge);
+    else {
+      groups.push(currentGroup = { edgeLength: decoratedEdge.edgeLength, edges: [decoratedEdge] });
+    }
+  });
+  return groups;
+};
---
+++ test/edgeProcessor.test.js
@@ -0,0 +1,103 @@
+import { expect } from 'chai';
+import Vertex from '../src/Vertex.js';
+import Vector3 from '../src/Vector.js';
+import { decorateEdges, groupDecoratedEdgesByLength } from '../src/edgeProcessor.js';
+
+describe('Edge processor', () => {
+  /** @type {Vertex} */ let vertex0;
+  /** @type {Vertex} */ let vertex1;
+  /** @type {Vertex} */ let vertex2;
+  /** @type {Vertex} */ let vertex3;
+
+  beforeEach(() => {
+    vertex0 = new Vertex('zero', new Vector3(1, 2, 3));
+    vertex1 = new Vertex('one', new Vector3(-2, 6, 3));
+    vertex2 = new Vertex('two', new Vector3(-4, 3, -3));
+    vertex3 = new Vertex('three', new Vector3(1, 5, -1));
+  });
+
+  describe('decorateEdges', () => {
+    it('should return an empty array when no edges are present', () => {
+      expect(decorateEdges(vertex0, -2)).to.be.an('array').with.lengthOf(0);
+    });
+
+    it('should process a single edge', () => {
+      vertex0.connect(vertex1);
+
+      const edges = decorateEdges(vertex0, -2);
+      const [edge] = edges;
+
+      expect(edges).to.be.an('array').with.lengthOf(1);
+      expect(edge.vectors[0].isEqualTo(vertex1.vector3)).to.be.true;
+      expect(edge.vectors[1].isEqualTo(vertex0.vector3)).to.be.true;
+      expect(edge.edgeLength).to.equal(5);
+      expect(edge.label).to.equal('one|zero');
+    });
+
+    it('should process multiple edges', () => {
+      vertex0.connect(vertex1.connect(vertex2)).connect(vertex2);
+
+      const edges = decorateEdges(vertex0, -2);
+
+      expect(edges).to.be.an('array').with.lengthOf(3);
+      expect(edges.map(({ vectors: [a, b] }) => [String(a), String(b)])).to.deep.equal([
+        [vertex1, vertex0],
+        [vertex2, vertex0],
+        [vertex1, vertex2],
+      ].map(([a, b]) => [String(a.vector3), String(b.vector3)]));
+      expect(edges.map(({ label, edgeLength }) => ({ label, edgeLength }))).to.deep.equal([
+        { label: 'one|zero', edgeLength: 5 },
+        { label: 'two|zero', edgeLength: 7.87 },
+        { label: 'one|two', edgeLength: 7 },
+      ]);
+
+      // Make sure all properties are being tested
+      expect(edges[0]).to.have.keys(['label', 'edgeLength', 'vectors']);
+      expect(edges[0].vectors).to.have.lengthOf(2);
+    });
+  });
+
+  describe('groupEdgesByLength', () => {
+    it('should return an empty array if given an empty array', () => {
+      expect(groupDecoratedEdgesByLength([])).to.be.an('array').that.is.empty;
+    });
+
+    it('should return one group with a single edge', () => {
+      vertex0.connect(vertex1);
+
+      const grouped = groupDecoratedEdgesByLength(decorateEdges(vertex0, 0));
+      const [group] = grouped;
+
+      expect(grouped).to.have.lengthOf(1);
+      expect(group.edgeLength).to.equal(5);
+      expect(group.edges).to.have.lengthOf(1);
+      expect(group.edges[0].vectors[0]).to.equal(vertex1.vector3);
+      expect(group.edges[0].vectors[1]).to.equal(vertex0.vector3);
+    });
+
+    it('should return one group with a multiple edges of same length', () => {
+      vertex0.connect(vertex1).connect(vertex3);
+
+      const grouped = groupDecoratedEdgesByLength(decorateEdges(vertex0, 0));
+      const [group] = grouped;
+
+      expect(grouped).to.have.lengthOf(1);
+      expect(group.edgeLength).to.equal(5);
+      expect(group.edges).to.have.lengthOf(2);
+      expect(group.edges.map((edge) => edge.label)).to.deep.equal(['one|zero', 'three|zero']);
+    });
+
+    it('should return multiple groups with edges of different lengths', () => {
+      vertex0.connect(vertex1.connect(vertex2)).connect(vertex2).connect(vertex3);
+      const grouped = groupDecoratedEdgesByLength(decorateEdges(vertex0, -2));
+
+      expect(grouped).to.have.lengthOf(3);
+      expect(grouped.map((group) => group.edgeLength)).to.deep.equal([5, 7, 7.87]);
+      expect(grouped.map((group) => group.edges.map((edge) => edge.label))).to.deep.equal([
+        ['one|zero', 'three|zero'],
+        ['one|two'],
+        ['two|zero'],
+      ]);
+    });
+  });
+});
