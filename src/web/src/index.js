40802f0b53201104cb31bb345c3019b65ef404af
Philip Enchin <Philip@Ench.in>
2024-05-28 16:07:56 +0000

    Render vertices and edges

--- src/web/src/index.js
+++ src/web/src/index.js
@@ -2,6 +2,7 @@ import p5 from 'p5';
 import { createIncrementor, createIncrementorUI } from './util.js';
 import { COLOR, UI } from './constants.js';
 import makeIcosahedron from '../../lib/util/icosahedron.js';
+import Vector3 from '../../lib/models/Vector.js';
 
 /** @typedef {import('./util.js').IncrementorUI} IncrementorUI */
 /** @typedef {import('../../lib/models/Vertex.js').default} Vertex */
@@ -19,7 +20,7 @@ const s = (sketch) => {
    * @returns {Vertex}
    */
   const makePolyhedron = () => {
-    const icosahedron = makeIcosahedron();
+    const icosahedron = makeIcosahedron().spherify('radius', 200);
     const subdividedIcosahedron = icosahedron.subdivide(frequencyUI.value);
 
     return subdividedIcosahedron;
@@ -38,9 +39,41 @@ const s = (sketch) => {
     polyhedron = makePolyhedron();
   };
 
+  const renderVertices = () => {
+    polyhedron.forEach(({ vector3: v }) => {
+      sketch.push();
+      sketch.translate(v.x, v.y, v.z);
+      sketch.sphere(15);
+      sketch.pop();
+    });
+  };
+
+  const renderEdges = () => {
+    const verticalVector = new Vector3(0, 1, 0);
+    polyhedron.edges.forEach(([{ vector3: a }, { vector3: b }]) => {
+      const edgeVector = b.minus(a);
+      const edgeLength = a.distanceTo(b);
+      const edgeCenter = a.plus(b).dividedBy(2);
+
+      const rotationVector = edgeVector.cross(verticalVector);
+      const rotationAngle = edgeVector.angleTo(verticalVector);
+
+      sketch.push();
+      sketch.translate(edgeCenter.x, edgeCenter.y, edgeCenter.z);
+      if (rotationVector.magnitude) sketch.rotate(-rotationAngle, [rotationVector.x, rotationVector.y, rotationVector.z]);
+
+      sketch.cylinder(15, edgeLength);
+      sketch.pop();
+    });
+  };
+
   sketch.draw = () => {
     sketch.background(COLOR.BACKGROUND);
     sketch.fill(255);
+
+    sketch.orbitControl();
+    renderVertices();
+    renderEdges();
   };
 };
 
