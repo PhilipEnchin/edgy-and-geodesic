fb29841f2b625f4c49c2c2f2b4c100f5d522ff88
Philip Enchin <Philip@Ench.in>
2024-05-29 18:33:30 +0000

    Refactor edge and vertex rendering

--- src/web/src/index.js
+++ src/web/src/index.js
@@ -2,78 +2,51 @@ import p5 from 'p5';
 import { createIncrementor, createIncrementorUI } from './util.js';
 import { COLOR, UI } from './constants.js';
 import makeIcosahedron from '../../lib/util/icosahedron.js';
-import Vector3 from '../../lib/models/Vector.js';
+import { edgeMap, vertexMap } from './coordinateMapping.js';
 
 /** @typedef {import('./util.js').IncrementorUI} IncrementorUI */
 /** @typedef {import('../../lib/models/Vertex.js').default} Vertex */
+/** @typedef {import('./coordinateMapping.js').Sketcher} Sketcher */
 
 const s = (sketch) => {
   const rowLocationIncrementor = createIncrementor(UI.MARGIN_TOP - UI.ROW_HEIGHT - UI.PADDING_INTER, -Infinity, Infinity, UI.ROW_HEIGHT + UI.PADDING_INTER);
 
   /** @type {IncrementorUI[]} */ const additionalUI = [];
 
-  let gui;
-
   /** @type {IncrementorUI} */ let frequencyUI;
 
-  /**
-   * @returns {Vertex}
-   */
-  const makePolyhedron = () => {
-    const icosahedron = makeIcosahedron().spherify('radius', 200);
-    const subdividedIcosahedron = icosahedron.subdivide(frequencyUI.value);
+  /** @type {Sketcher[]} */ let vertexSketchers;
+  /** @type {Sketcher[]} */ let edgeSketchers;
+  const updatePolyhedron = () => {
+    let polyhedron = makeIcosahedron().spherify('radius', 200);
+    if (frequencyUI.value > 1) polyhedron = polyhedron.subdivide(frequencyUI.value);
 
-    return subdividedIcosahedron;
+    vertexSketchers = vertexMap(polyhedron, 15);
+    edgeSketchers = edgeMap(polyhedron, 15);
   };
-
-  /** @type {Vertex} */ let polyhedron;
-  const updatePolyhedron = () => { polyhedron = makePolyhedron(); };
 
   const simpleLayout = () => {
     additionalUI.push(frequencyUI = createIncrementorUI(sketch, 'Frequency', 1, 1, 10, 1, 10, rowLocationIncrementor.increment().value, updatePolyhedron));
   };
 
   sketch.setup = () => {
-    const canvas = sketch.createCanvas(600, 600, sketch.WEBGL);
+    sketch.createCanvas(600, 600, sketch.WEBGL);
     simpleLayout();
-    polyhedron = makePolyhedron();
+    updatePolyhedron();
   };
 
-  const renderVertices = () => {
-    polyhedron.forEach(({ vector3: v }) => {
-      sketch.push();
-      sketch.translate(v.x, v.y, v.z);
-      sketch.sphere(15);
-      sketch.pop();
-    });
-  };
-
-  const renderEdges = () => {
-    const verticalVector = new Vector3(0, 1, 0);
-    polyhedron.edges.forEach(([{ vector3: a }, { vector3: b }]) => {
-      const edgeVector = b.minus(a);
-      const edgeLength = a.distanceTo(b);
-      const edgeCenter = a.plus(b).dividedBy(2);
-
-      const rotationVector = edgeVector.cross(verticalVector);
-      const rotationAngle = edgeVector.angleTo(verticalVector);
-
-      sketch.push();
-      sketch.translate(edgeCenter.x, edgeCenter.y, edgeCenter.z);
-      if (rotationVector.magnitude) sketch.rotate(-rotationAngle, [rotationVector.x, rotationVector.y, rotationVector.z]);
-
-      sketch.cylinder(15, edgeLength);
-      sketch.pop();
-    });
-  };
+  /**
+   * @param {Sketcher[]} sketcher
+   */
+  const renderSketchers = (sketcher) => { sketcher.forEach(({ method, args }) => sketch[method](...args)); };
 
   sketch.draw = () => {
     sketch.background(COLOR.BACKGROUND);
     sketch.fill(255);
 
     sketch.orbitControl();
-    renderVertices();
-    renderEdges();
+    renderSketchers(vertexSketchers);
+    renderSketchers(edgeSketchers);
   };
 };
 
